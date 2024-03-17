4e257b0854f04990d3ea759cdd549e61286afda5
Narayanaswamy <narayanaswamy87@gmail.com>
2024-03-17 18:52:28 +0530

    Added Employee class

---
+++ src/main/java/com/javatechie/dockerjenkinsintegration/Employee.java
@@ -0,0 +1,14 @@
+package com.javatechie.dockerjenkinsintegration;
+
+public class Employee {
+    public int id;
+    public String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
